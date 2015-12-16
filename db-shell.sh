e1f1cd5d8fba136db85220670046d2d473eee2b5
PsychoticSmurf <hohesc@hohesc.us>
2015-12-16 17:23:34 +0100

    no comment

--- db-shell.sh
+++ db-shell.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
 container=$(echo ${PWD##*/}| sed 's/-//')
 docker run -it --link ${container}_db_1:mysql --rm mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD"'
-=======
-docker run -it --link galileo-docker_db_1:mysql --rm mysql sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD"'
->>>>>>> a438aba026793fc7d3ba0f46ae53d3d28800dd52
