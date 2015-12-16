7ce5f1ecdb211de81a97ae729853787f0bfc0d3a
PsychoticSmurf <hohesc@hohesc.us>
2015-12-16 17:20:50 +0100

    change db scripts

--- db-seed.sh
+++ db-seed.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 docker build -t tripwire-seed sql/
+<<<<<<< HEAD
+container=$(echo ${PWD##*/}| sed 's/-//')
+docker run --rm --link ${container}_db_1:mysql tripwire-seed
+=======
 docker run --rm --link galileo-docker_db_1:mysql tripwire-seed
+>>>>>>> a438aba026793fc7d3ba0f46ae53d3d28800dd52
